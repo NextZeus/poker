a06e9c02d6867c155b72f5a36f4e3f397e703204
小东 <875697126@qq.com>
2016-03-31 12:38:49 +0800

    help pageview
    make help page view scene

--- src/app.js
+++ src/app.js
@@ -4,14 +4,6 @@ var LoginLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
         var size = cc.winSize;
-        //var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        //// position the label on the center of the screen
-        //helloLabel.x = size.width / 2;
-        //helloLabel.y = size.height / 2 + 200;
-        //// add the label as a child to this layer
-        //this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
         this.sprite = new cc.Sprite(res.login_png);
         this.sprite.attr({
             x: size.width / 2,
@@ -31,7 +23,7 @@ var LoginScene = cc.Scene.extend({
 
         setTimeout(function () {
             cc.director.runScene(new HelpScene());
-        },2000)
+        },1000)
     }
 });
 
--- src/help/help.js
+++ src/help/help.js
@@ -7,22 +7,51 @@ var HelpLayer = cc.Layer.extend({
         this.initHelpImage();
     },
     initHelpImage: function () {
-        var help1 = new cc.Sprite(res.help_1_png);
-        var help2 = new cc.Sprite(res.help_2_png);
-        var help3 = new cc.Sprite(res.help_3_png);
+        var pageView = new ccui.PageView();
 
-        var size = cc.director.getWinSize();
+        var size = cc.winSize;
 
-        this.addChild(help1);
-        help1.x = size.width  / 2;
-        help1.y = size.height / 2;
-        //this.addChild(help2);
-        //help2.x = size.width  / 2;
-        //help2.y = size.height / 2;
-        //this.addChild(help3);
-        //help3.x = size.width  / 2;
-        //help3.y = size.height / 2;
+        pageView.setTouchEnabled(true);
+        pageView.setContentSize(cc.size(750,1334));
+        pageView.setAnchorPoint(cc.p(0.5,0.5));
+        pageView.x = size.width / 2;
+        pageView.y = size.height / 2;
+
+
+        for(var i = 0 ; i < 3; i++){
+            var layout = new ccui.Layout();
+
+            var imageView = new ccui.ImageView();
+            imageView.loadTexture(res['help_'+i+'_png']);
+            imageView.x = pageView.width / 2;
+            imageView.y = pageView.height / 2;
+            layout.addChild(imageView);
+
+            var text = new ccui.Text();
+
+            text.string = "Page "+ (i + 1);
+            text.font = "100px 'Marker Felt'";
+            text.color = cc.color(192,192,192);
+            text.x = pageView.width / 2;
+            text.y = pageView.height / 2 + 300;
+            layout.addChild(text);
+
+            pageView.addPage(layout);
+
+        }
+
+        pageView.addEventListener(this.pageViewEvent,this);
+        this.addChild(pageView);
+
         return true;
+    },
+    pageViewEvent: function (sender,type) {
+        switch (type){
+            case ccui.PageView.EVENT_TURNING:
+                cc.log("Page: "+sender.getCurPageIndex());
+                break;
+
+        }
     }
 
 });
